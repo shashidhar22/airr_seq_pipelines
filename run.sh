81f18e05f76af31d7526c6727a34e64dbba81798
Shashidhar Ravishankar <shashidhar.r.shankar@gmail.com>
2021-09-08 12:07:21 -0700

    Run script

---
+++ run.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -Eeuo pipefail
+
+# Nextflow Version
+NXF_VER=20.10.0
+
+# Nextflow Configuration File
+NXF_CONFIG=nextflow.config
+
+# Workflow to Run (e.g. GitHub repository)
+WORKFLOW_REPO=main.nf #shashidhar22/airr_seq_pipelines
+
+# Queue to use for exeution
+QUEUE=campus-new
+
+# Define scratch directory
+SCRATCH=path_to_scratch_directory
+
+# Define params file
+PARAMS=path_to_params_file
+
+# Load the Nextflow module (if running on rhino/gizmo)
+ml Nextflow
+
+# Load the Singularity module (if running on rhino/gizmo with Singularity)
+ml Singularity
+
+# Load Conda module (if running on rhino/gizmo with Conda)
+ml Anaconda3
+# Make sure that the singularity executables are in the PATH
+export PATH=$SINGULARITYROOT/bin/:$PATH
+
+# Run the workflow
+NXF_VER=$NXF_VER \
+nextflow \
+    run \
+    -c ${NXF_CONFIG} \
+    ${WORKFLOW_REPO} \
+    -entry gliphRunner \
+    -w  ${SCRATCH} \
+    -with-tower \
+    -params-file ${PARAMS} \
+    -resume
